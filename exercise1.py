0d7284a1a11c5caa47624728d8be2290dd99b732
adam-weiler <adam.weiler@yahoo.ca>
2019-07-10 16:57:35 -0400

    Exercise 1 complete.

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+#Exercise 1
+
+def double(my_number): #Returns the double of my_number.
+    return my_number * 2
+
+print(double(2)) #Returns 4
+print(double(6)) #Returns 12
+print(double(42)) #Returns 84
+print(double(3.1415926)) #Returns 6.2831852
